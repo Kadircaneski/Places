67b222b92ffdf700e51668475f60ab5b7037b880
Kadircan Eski <kadircaneski@hotmail.com>
2023-05-16 21:47:54 +0300

    location fixed

--- lib/widgets/location.input.dart
+++ lib/widgets/location.input.dart
@@ -1,99 +1,99 @@
-// import 'package:flutter/material.dart';
-// import 'package:location/location.dart';
-// // import 'package:http/http.dart' as http;
+import 'package:flutter/material.dart';
+import 'package:location/location.dart';
+// import 'package:http/http.dart' as http;
 
-// class LocationInput extends StatefulWidget {
-//   const LocationInput({super.key});
+class LocationInput extends StatefulWidget {
+  const LocationInput({super.key});
 
-//   @override
-//   State<LocationInput> createState() => _LocationInputState();
-// }
+  @override
+  State<LocationInput> createState() => _LocationInputState();
+}
 
-// class _LocationInputState extends State<LocationInput> {
-//   Location? _pickedLocation;
-//   var _isGettingLocation = false;
+class _LocationInputState extends State<LocationInput> {
+  Location? _pickedLocation;
+  var _isGettingLocation = false;
 
-//   void _getCurrentLocation() async {
+  void _getCurrentLocation() async {
     
-//     Location location = Location();
+    Location location = Location();
 
-//     bool serviceEnabled;
-//     PermissionStatus permissionGranted;
-//     LocationData locationData;
+    bool serviceEnabled;
+    PermissionStatus permissionGranted;
+    LocationData locationData;
 
-//     serviceEnabled = await location.serviceEnabled();
-//     if (!serviceEnabled) {
-//       serviceEnabled = await location.requestService();
-//       if (!serviceEnabled) {
-//         return;
-//       }
-//     }
+    serviceEnabled = await location.serviceEnabled();
+    if (!serviceEnabled) {
+      serviceEnabled = await location.requestService();
+      if (!serviceEnabled) {
+        return;
+      }
+    }
 
-//     permissionGranted = await location.hasPermission();
-//     if (permissionGranted == PermissionStatus.denied) {
-//       permissionGranted = await location.requestPermission();
-//       if (permissionGranted != PermissionStatus.granted) {
-//         return;
-//       }
-//     }
-//     setState(() {
-//       _isGettingLocation = true;
-//     });
+    permissionGranted = await location.hasPermission();
+    if (permissionGranted == PermissionStatus.denied) {
+      permissionGranted = await location.requestPermission();
+      if (permissionGranted != PermissionStatus.granted) {
+        return;
+      }
+    }
+    setState(() {
+      _isGettingLocation = true;
+    });
 
-//     locationData = await location.getLocation();
-//     // final url = Uri.parse();
-//     setState(() {
-//       _isGettingLocation = false;
-//     });
-//     print(locationData.longitude);
-//     print(locationData.latitude);
-//   }
+    locationData = await location.getLocation();
+    // final url = Uri.parse();
+    setState(() {
+      _isGettingLocation = false;
+    });
+    print(locationData.longitude);
+    print(locationData.latitude);
+  }
 
-//   @override
-//   Widget build(BuildContext context) {
-//     Widget previewContent = Text(
-//       'No location chosen.',
-//       textAlign: TextAlign.center,
-//       style: Theme.of(context)
-//           .textTheme
-//           .bodyLarge!
-//           .copyWith(color: Theme.of(context).colorScheme.onBackground),
-//     );
+  @override
+  Widget build(BuildContext context) {
+    Widget previewContent = Text(
+      'No location chosen.',
+      textAlign: TextAlign.center,
+      style: Theme.of(context)
+          .textTheme
+          .bodyLarge!
+          .copyWith(color: Theme.of(context).colorScheme.onBackground),
+    );
 
-//     if (_isGettingLocation) {
-//       previewContent = const CircularProgressIndicator();
-//     }
+    if (_isGettingLocation) {
+      previewContent = const CircularProgressIndicator();
+    }
 
-//     return Column(
-//       children: [
-//         Container(
-//           alignment: Alignment.center,
-//           height: 170,
-//           width: double.infinity,
-//           decoration: BoxDecoration(
-//             border: Border.all(
-//               width: 1,
-//               color: Theme.of(context).colorScheme.primary.withOpacity(0.2),
-//             ),
-//           ),
-//           child: previewContent,
-//         ),
-//         Row(
-//           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-//           children: [
-//             TextButton.icon(
-//               onPressed: _getCurrentLocation,
-//               icon: const Icon(Icons.location_on),
-//               label: const Text('Get Current Location'),
-//             ),
-//             TextButton.icon(
-//               onPressed: () {},
-//               icon: const Icon(Icons.map),
-//               label: const Text('Select on Map'),
-//             ),
-//           ],
-//         )
-//       ],
-//     );
-//   }
-// }
+    return Column(
+      children: [
+        Container(
+          alignment: Alignment.center,
+          height: 170,
+          width: double.infinity,
+          decoration: BoxDecoration(
+            border: Border.all(
+              width: 1,
+              color: Theme.of(context).colorScheme.primary.withOpacity(0.2),
+            ),
+          ),
+          child: previewContent,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            TextButton.icon(
+              onPressed: _getCurrentLocation,
+              icon: const Icon(Icons.location_on),
+              label: const Text('Get Current Location'),
+            ),
+            TextButton.icon(
+              onPressed: () {},
+              icon: const Icon(Icons.map),
+              label: const Text('Select on Map'),
+            ),
+          ],
+        )
+      ],
+    );
+  }
+}
